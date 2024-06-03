06fc0b55f251b5c42ada906ba91a749962f1d2f0
Gabriel Guerra <gabrielguerra190803@gmail.com>
2024-06-03 19:45:45 -0300

    ultimas atualizações no DataBase

--- src/database/mongo.database.js
+++ src/database/mongo.database.js
@@ -3,12 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () =>{
 
-     mongoose.connect( process.env.MONGO_URI,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        }
-    )
+     mongoose.connect( process.env.MONGO_URI)
 
 .then(() => {console.log("Conectado ao banco de dados")})
 
