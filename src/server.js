f1bee89a934048776fefa7f6f00b5c73ee5c1022
Gabriel Guerra <gabrielguerra190803@gmail.com>
2024-06-03 21:32:30 -0300

    adição do mimdlwares cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const swaggerUI = require("swagger-ui-express");
 const dotEnv = require("dotenv");
+const cors = require("cors");
 
 const connectDatabase = require("./database/mongo.database.js")
 const swaggerDocs = require("../swaggerConfig/swagger.json");
@@ -13,6 +14,7 @@ const app = express();
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 connectDatabase();
 app.use(express.json());
+app.use(cors());
 app.use("/NutriLife/api",route);
 
 app.listen(PORT, () => console.log(`servidor rodando na porta: ${PORT}`));
