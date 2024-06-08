20fce4bea9e6c2e833ea3a7795099d23f329d6b2
Gabriel Guerra <gabrielguerra190803@gmail.com>
2024-06-08 15:01:15 -0300

    inclusao de uma autenticação dos usuários

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,39 @@
+const bcrypt = require ("bcrypt");
+const User = require("../models/user.model");
+
+
+const login  = async (req,res) => {
+    try{
+        const {email , password} = req.body;
+
+        const user = await loginService(email);
+
+        if(!user){
+            console.error("email ou senha incorreta", error);
+            res.status(400).json({ message: error.message });
+        }
+
+
+        const passwordIsValid = await bcrypt.compare(password,user.password)
+
+        if(!passwordIsValid){
+            console.error("email ou senha incorreta", error);
+            res.status(400).json({ message: error.message });
+        }
+
+        res.send(user)
+    
+    
+    }catch(error){
+        console.error("usuário não encontardo", error);
+        res.status(500).json({ message: error.message });
+    } 
+    
+}
+
+
+const loginService = (email) => 
+    User.findOne({email: email}).select("+password");
+
+
+module.exports = login
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 
 const userScheme = mongoose.Schema({
     email:{
         type: String,
+        unique: true,
         required: true
     },
     name:{
@@ -16,9 +17,17 @@ const userScheme = mongoose.Schema({
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        select: false
     },
 })
+
+
+userScheme.pre("save", async function (next){
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
 
 const User = mongoose.model("User",userScheme);
 
