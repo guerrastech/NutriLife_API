f154ea2759fb83119a76c457c39814e7afd0cdf6
Gabriel Guerra <gabrielguerra190803@gmail.com>
2024-06-08 21:04:43 -0300

    atualização no controller da autenticação

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,39 +1,31 @@
-const bcrypt = require ("bcrypt");
+const bcrypt = require("bcrypt");
 const User = require("../models/user.model");
 
-
-const login  = async (req,res) => {
-    try{
-        const {email , password} = req.body;
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
 
         const user = await loginService(email);
 
-        if(!user){
-            console.error("email ou senha incorreta", error);
-            res.status(400).json({ message: error.message });
+        if (!user) {
+            console.error("Usuário não encontrado");
+            return res.status(400).json({ message: "Email ou senha incorreta" });
         }
 
+        const passwordIsValid = await bcrypt.compare(password, user.password);
 
-        const passwordIsValid = await bcrypt.compare(password,user.password)
-
-        if(!passwordIsValid){
-            console.error("email ou senha incorreta", error);
-            res.status(400).json({ message: error.message });
+        if (!passwordIsValid) {
+            console.error("Senha incorreta");
+            return res.status(400).json({ message: "Email ou senha incorreta" });
         }
 
-        
-        res.status(200).send(user)
-    
-    }catch(error){
-        console.error("usuário não encontardo", error);
-        res.status(500).json({ message: error.message });
-    } 
-    
-}
+        res.status(200).json(user);
+    } catch (error) {
+        console.error("Erro ao tentar fazer login:", error);
+        res.status(500).json({ message: "Erro interno do servidor" });
+    }
+};
 
+const loginService = (email) => User.findOne({ email: email }).select("+password");
 
-const loginService = (email) => 
-    User.findOne({email: email}).select("+password");
-
-
-module.exports = login
+module.exports = login;
